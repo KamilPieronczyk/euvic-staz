95135c8d49426d4dcf1e1568c7d2fc05dc8b36f5
Kamil <kamilpier08@gmail.com>
2021-04-06 23:19:55 +0200

    footer img

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -4,18 +4,12 @@ import './Body.css'
 export function Body() {
   return (
     <div className={'bodyContainer'}>
-      {/* <div className={'bodyImage'}>
-        img
-      </div> */}
-      <div className={'inside'}>
-        Body
-        <p>ss</p>
-        <p>ss</p>
-        <p>ss</p>
-        <p>ss</p>
-        <p>ss</p>
-        <p>ss</p>
-      </div>
+      Body
+      <p>ss</p>
+      <p>ss</p>
+      <p>ss</p>
+      <p>ss</p>
+      <p>ss</p>
     </div>
   )
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,9 @@ import {FooterMenu} from './FooterMenu/FooterMenu'
 export function Footer() {
   return (
     <footer className={"footerContainer"}>
+      <div className={"footerImg"}>
+        <img src="/assets/background.jpg" alt="Sea"/>
+      </div>
       <FooterMenu />
       <div id="footerRights">
         <p>@2021 NextTrip. All rights reserved</p>
